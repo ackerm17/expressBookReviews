b5a98345536a5c08588064bf59cfaa1b4f38f858
Benjamin Ackerman <115006023+ackerm17@users.noreply.github.com>
2023-10-28 16:34:10 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,40 +26,34 @@ public_users.post("/register", (req,res) => {
 
     
 
-axios.get('http://webcode.me').then(resp => {
+axios.get('/').then(resp => {
     res.send(JSON.stringify(books,null,4));
     
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    new Promise((resolve, reject) => {
+axios.get('/isbn/:isbn').then(resp => {
         const isbn = req.params.isbn;
         res.send(books[isbn])
-    })
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    new Promise((resolve, reject) => {    
+axios.get('/author/:author').then(resp => {
         const author = req.params.author;
         for(let i = 1; i<=10; i++){
             if(author == books[i].author){
                 res.send(books[i])
             }
-        }
     })
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    new Promise((resolve, reject) => {    
+axios.get('/title/:title').then(resp => {
         const title = req.params.title;
         for(let i = 1; i<=10; i++){
             if(title == books[i].title){
                 res.send(books[i])
             }
-        }
     })
 });
 
